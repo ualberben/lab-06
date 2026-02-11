ff6e94025732941dae66a436bc6a8de4a54614d6
ualberben <pinjui1@ualberta.ca>
2026-02-10 20:43:49 -0700

    lab in class

---
+++ app/src/main/java/com/example/ListyCity/City.java
@@ -0,0 +1,28 @@
+package com.example.ListyCity;
+
+/**
+ * This is a Class that define a city
+ */
+public class City implements Comparable<City>{
+    private String city;
+    private String province;
+
+    City(String city, String province){
+        this.city = city;
+        this.province = province;
+    }
+
+    String getCityName(){
+        return this.city;
+    }
+
+    String getProvinceName(){
+        return this.province;
+    }
+
+    @Override
+    public int compareTo(City o) {
+        City city = (City) o;
+        return this.city.compareTo(city.getCityName());
+    }
+}
---
+++ app/src/main/java/com/example/ListyCity/CityList.java
@@ -0,0 +1,36 @@
+package com.example.ListyCity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This is a class that keeps track of a list of city objects
+ */
+public class CityList {
+    private List<City> cities = new ArrayList<>();
+
+    /**
+     * This adds a city to list if city does not exist
+     * @param city
+     *      This is a candidate city to add
+     */
+    public void add(City city){
+        if(cities.contains(city)){
+            throw new IllegalArgumentException();
+        }
+        cities.add(city);
+    }
+
+    /**
+     * This returns a sorted list of cities
+     * @return
+     *      Return the sorted list
+     */
+    public List<City> getCities(){
+        List<City> list = cities;
+        Collections.sort(list);
+        return list;
+    }
+}
